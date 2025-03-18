c44d611dfbf95f5b15f04944b995b4b6d5796c5e
Vladyslav Hirchuk <145594568+SoloScriptSage@users.noreply.github.com>
2025-03-18 03:22:11 +0100

    Update ram_monitor.h

--- winapi/ram_monitor.h
+++ winapi/ram_monitor.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include <windows.h>
+#include "alerts.h"
+#include <chrono>
+#include <atomic>
+#include <thread>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+using namespace std;
+
+// Define a memory alert threshold
+#define MEMORY_ALERT_THRESHOLD 85 // Set the alert threshold to 85%
+
+// Function declarations
+void GetMemoryUsage(HWND hWND); // Function to get the current memory usage
+void UpdateMemoryUsage(); // Function to run in a separate thread for continuous memory monitoring
+bool ShouldShowAlert(std::chrono::steady_clock::time_point& lastAlertTime); // Check if enough time has passed since the last alert
