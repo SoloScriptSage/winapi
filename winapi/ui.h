c5b58613415383554994d3f6df355b5a09468293
Vladyslav Hirchuk <145594568+SoloScriptSage@users.noreply.github.com>
2025-03-18 03:23:17 +0100

    Update ui.h

--- winapi/ui.h
+++ winapi/ui.h
@@ -1,1 +1,29 @@
-#pragma once
+// ui.h
+#ifndef UI_H
+#define UI_H
+
+#include <windows.h>
+
+// Global variables for UI controls
+extern HWND hStaticControl;
+extern HWND hEditControl;
+extern HWND hNumberControl;
+extern HWND hCPU;
+extern HWND hRAM;
+extern HWND hDisk;
+extern HWND hNetwork;
+extern RECT rc;
+
+#define DIGIT_INDEX_NUMBER 200
+
+#define DLG_INDEX_COLOR_R 201
+#define DLG_INDEX_COLOR_G 202
+#define DLG_INDEX_COLOR_B 203
+
+#define BTN_READ_CLR 1005
+
+// Function declarations
+void MainWndAddMenus(HWND hWnd);
+void MainWndAddWidgets(HWND hWnd);
+
+#endif // UI_H
