1488e5c69bc04de459dc539848ac58209a1fbd52
Vladyslav Hirchuk <145594568+SoloScriptSage@users.noreply.github.com>
2025-03-18 03:20:56 +0100

    Update disk_monitor.h

--- winapi/disk_monitor.h
+++ winapi/disk_monitor.h
@@ -1,1 +1,7 @@
 #pragma once
+#include <windows.h>     // For Windows API functions (GetDiskFreeSpaceExA, SetWindowTextA, MessageBox)
+#include <thread>        // For std::this_thread::sleep_for
+#include <chrono>        // For chrono::seconds
+#include <cstdio>        // For snprintf
+#include <string>        // Optional: If you use string manipulation (though it's not directly used in the given code)
+#include <atomic>
