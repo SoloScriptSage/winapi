c6e5e6b5bfad7afeaeea76916cd52c57c5b59aa3
Vladyslav Hirchuk <145594568+SoloScriptSage@users.noreply.github.com>
2025-03-18 03:19:49 +0100

    Update alerts.h

--- winapi/alerts.h
+++ winapi/alerts.h
@@ -4,7 +4,12 @@
 #include <Windows.h>
 #include <chrono>
 
+#define CPU_ALERT_THRESHOLD 80 // CPU usage alert threshold
+#define MEMORY_ALERT_THRESHOLD 80 // Memory usage alert threshold
+#define DISK_ALERT_THRESHOLD 80 // Disk usage alert threshold
+
 bool ShouldShowAlert(std::chrono::steady_clock::time_point& lastAlertTime);
 void CheckAndShowAlerts();
+void AlertThread();
 
 #endif // ALERTS_H
